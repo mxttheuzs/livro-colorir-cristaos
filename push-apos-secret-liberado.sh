fb1b2c850bb363ac01b465b6b5552ab0a12c0dd3
mxttheuzs <math11discord@gmail.com>
2025-07-27 03:09:58 +0000

    Push projeto livros colorir Sun Jul 27 03:09:58 AM UTC 2025

---
+++ push-apos-secret-liberado.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "=== PUSH FINAL APÓS SECRET LIBERADO ==="
+echo ""
+echo "O GitHub liberou o secret! Agora faça o push final:"
+echo ""
+echo "Execute no Shell do Replit:"
+echo ""
+echo "git push https://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@github.com/mxttheuzs/livro-colorir-cristaos.git main"
+echo ""
+echo "Ou se preferir verificar status primeiro:"
+echo ""
+echo "git status"
+echo "git push https://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@github.com/mxttheuzs/livro-colorir-cristaos.git main"
